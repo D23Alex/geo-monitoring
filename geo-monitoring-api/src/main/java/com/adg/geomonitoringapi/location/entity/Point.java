5523decf21ec62bcef533c5f90b0b0a1f03a9d53
ThisAster <freizen034@mail.ru>
2025-03-13 12:55:09 +0300

    add: point entity

---
+++ geo-monitoring-api/src/main/java/com/adg/geomonitoringapi/location/entity/Point.java
@@ -0,0 +1,26 @@
+package com.adg.geomonitoringapi.location.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int width;
+    private int length;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+}
